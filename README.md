d5ceeb3b4143c35f42b11f4480af733130634501
Yassine AZZOUT <92bondstreet@gmail.com>
2016-08-22 22:09:57 +0200

    feat(script): install automagically elk

---
+++ README.md
@@ -0,0 +1,30 @@
+# genesis-elk
+
+> Automagic installation of ELK with nginx as proxy into Kibana and authentication layer on top
+
+
+## Usage
+
+**Note:** Root only
+
+Scripts
+
+```sh
+❯ git clone https://github.com/92bondstreet/genesis-elk.git
+❯ cd genesis-elk
+❯ chmod a+x genesis-elk.sh
+❯ sh genesis-elk.sh
+```
+
+or
+
+```sh
+❯ wget --no-check-certificate https://raw.github.com/92bondstreet/genesis-elk/master/genesis-elk.sh
+❯ chmod a+x genesis-elk.sh
+❯ sh genesis-elk.sh
+
+```
+
+## Deep diving
+
+* [The ELK stack powered by Docker and Compose](https://github.com/deviantony/docker-elk)
---
+++ genesis-elk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Custom echo
+red='\033[0;31m'
+yellow='\033[0;33m'
+green='\033[0;32m'
+
+## Color-echo.
+#  Reset text attributes to normal + without clearing screen.
+alias Reset="tput sgr0"
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}"
+  Reset # Reset to normal.
+  return
+}
+
+# Display header
+header() {
+  cecho "------------------------------------------------------------------------------" $yellow
+  cecho "$*" $yellow
+  cecho "------------------------------------------------------------------------------" $yellow
+
+}
+
+# Who are you?
+if [ "$(id -u)" != "0" ]; then
+  cecho "Genesis is for ROOT" $red
+  cecho "❯ su" $red
+  exit 1
+fi
+
+header "Install ELK stack"
+docker-compose up
+cecho "ELK stack done" $green
